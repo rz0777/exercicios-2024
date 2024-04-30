79006e94128d65b2d78ff837d1c703cd5449ec3c
rz0777 <otaviorzampolli@gmail.com>
2024-04-30 18:13:18 -0300

    base scrap

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -5,6 +5,8 @@ namespace Chuva\Php\WebScrapping;
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
 
+use DOMXPath;
+
 /**
  * Does the scrapping of a webpage.
  */
@@ -14,17 +16,13 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+    $xpath = new DOMXpath($dom);
+    $papers = $xpath->query("//a[contains(@class, 'paper-card']");
+
+    foreach ($papers as $paper) {
+      $id->$xpath->query("//div[contains(@class,'volume-info'");
+      
+    }
   }
 
 }
