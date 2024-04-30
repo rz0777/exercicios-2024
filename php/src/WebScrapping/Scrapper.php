7b291f2d3f7e88155b8a7c0b21472c2bebd44221
rz0777 <otaviorzampolli@gmail.com>
2024-04-30 18:31:03 -0300

    scrap com return

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -17,12 +17,31 @@ class Scrapper {
    */
   public function scrap(\DOMDocument $dom): array {
     $xpath = new DOMXpath($dom);
-    $papers = $xpath->query("//a[contains(@class, 'paper-card']");
+    $papers = $xpath->query(".//a[contains(@class, 'paper-card']");
+    $papers_return = [];
 
     foreach ($papers as $paper) {
-      $id->$xpath->query("//div[contains(@class,'volume-info'");
-      
+      $id->$xpath->query(".//div[contains(@class,'volume-info'",$paper)[0]->textContent;
+
+      $title->$xpath->query(".//h4[contains(@class,'paper-title')]",$paper)[0]->textContent;
+      $tag->$xpath->query(".//div[contains(@class,'tags mr-sm')]",$paper)[0]->textContent;
+
+      $authors_paper = [];
+      $authors_div->$xpath->query(".//div[contains(@class,'volume-info'",$paper);
+      $authors->$xpath->query(".//span",$authors_div);
+
+      foreach($authors as $author){
+        $person = new Person($authors->textContent,$author->getAttribute("title"));
+        array_push($authors_paper[],$person);
+      }
+
+
+      $paper_scrap = new Paper($id,$title,$tag,$authors_paper);
+      array_push($papers_return,$paper_scrap);
+
+
     }
+      return $papers_return;
   }
 
 }
