0335052d7b37d875935ef28bd2f9612f47c7f83f
rz0777 <otaviorzampolli@gmail.com>
2024-05-02 10:50:15 -0300

    criacao planilha finalizada

--- php/src/WebScrapping/SproutWriter.php
+++ php/src/WebScrapping/SproutWriter.php
@@ -24,6 +24,9 @@
             $filePath = __DIR__ . '../../../assets/planilha.xlsx';
             $writer->openToFile($filePath);
 
+            $row = WriterEntityFactory::createRowFromArray(['ID', 'Title', 'Type', 'Author 1', 'Author 1 Institution', 'Author 2', 'Author 2 Institution', 'Author 3', 'Author 3 Institution', 'Author 4', 'Author 4 Institution', 'Author 5', 'Author 5 Institution', 'Author 6', 'Author 6 Institution', 'Author 7', 'Author 7 Institution', 'Author 8', 'Author 8 Institution', 'Author 9', 'Author 9 Institution']);
+            $writer->addRow($row);
+
             foreach ($papers as $paper) {
                 $data = [
                     $paper->id,
